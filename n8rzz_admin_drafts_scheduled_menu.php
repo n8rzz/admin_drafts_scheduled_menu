b68969714937729184c756f381d5a66029646708
Nate Geslin <teamtomkins23@gmail.com>
2023-12-19 17:37:00 -0600

    moves to one-param-per-line for readability

--- n8rzz_admin_drafts_scheduled_menu.php
+++ n8rzz_admin_drafts_scheduled_menu.php
@@ -11,14 +11,24 @@
 // see: https://davidwalsh.name/wordpress-admin-add-menu-item
 function add_drafts_admin_menu_item() {
   // $page_title, $menu_title, $capability, $menu_slug, $callback_function
-  add_posts_page(__('Drafts'), __('Drafts'), 'read', 'edit.php?post_status=draft&post_type=post&orderby=date&order=desc');
+  add_posts_page(
+    __('Drafts'), 
+    __('Drafts'), 
+    'read', 
+    'edit.php?post_status=draft&post_type=post&orderby=date&order=desc'
+  );
 }
 
 add_action('admin_menu', 'add_drafts_admin_menu_item');
 
 
 function add_scheduled_admin_menu_item() {
-  add_posts_page(__('Scheduled'), __('Scheduled'), 'read', 'edit.php?post_status=future&post_type=post&orderby=date&order=desc');
+  add_posts_page(
+    __('Scheduled'), 
+    __('Scheduled'), 
+    'read', 
+    'edit.php?post_status=future&post_type=post&orderby=date&order=desc'
+  );
 }
 
 add_action('admin_menu', 'add_scheduled_admin_menu_item');
